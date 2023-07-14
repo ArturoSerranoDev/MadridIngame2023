e08540c432bfeea8b799ebaa0e4b88a66edbcbe2
ArturoSerranoDev <asm.arturo.developer@gmail.com>
2023-07-14 23:59:58 +0200

    WIP - Overall progress in level transitions

--- Assets/_Project/_Scripts/Controllers/MainController.cs
+++ Assets/_Project/_Scripts/Controllers/MainController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using TMPro;
 
 public enum GameState
 {
@@ -16,32 +18,97 @@ public enum GameState
 
 public class MainController : MonoBehaviour
 {
+    [Header("Dependencies")]
     [SerializeField] private InputController inputController;
+    
     [SerializeField] private GameObject mainCanvas;
+    [SerializeField] private GameObject marcoAlrededorEscena;
+
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI sceneTitleText;
+    
+    [SerializeField] private GameObject testPuertaIzquierda;
+    [SerializeField] private GameObject testPuertaDerecha;
     
     [SerializeField] private GameState gameState = GameState.MainMenu;
     [SerializeField] private List<BaseScene> gameScenes;
-
-    [SerializeField] private BaseScene testScene;
-
+    
+    [Header("Parameters")]
+    
     private int _currentSceneIndex = 0;
     
+    private void Start()
+    {
+        scoreText.gameObject.SetActive(false);
+        sceneTitleText.gameObject.SetActive(false);
+    }
     
     public void OnPlayButtonPressed()
     {
+        // INTRO DEL JUEGO
         mainCanvas.gameObject.SetActive(false);
-
-        StartMinigame(0);
+        
+        InitMinigame(0);
+        
+        StartCoroutine(TransitionInMinigameCoroutine());
     }
-    public void StartMinigame(int sceneIndex)
+    private IEnumerator TransitionInMinigameCoroutine()
+    {
+        // Fade in Puerta cerrada
+        marcoAlrededorEscena.gameObject.SetActive(true);
+        foreach (Transform child in marcoAlrededorEscena.transform)
+        {
+            child.GetComponent<Renderer>().material.DOFade(1f, 0.5f).From(0f);
+        }
+        yield return new WaitForSeconds(0.5f);
+        
+        // Puerta abre (in black)
+        testPuertaIzquierda.transform.DOMoveX(-6f, 1f).SetEase(Ease.InOutSine);
+        testPuertaDerecha.transform.DOMoveX(6f, 1f).SetEase(Ease.InOutSine);
+        yield return new WaitForSeconds(1f);
+
+        // Fade in Controllers si no se han explicado
+        
+        // Fade out Controllers
+        
+        // Animar Score, win or lose
+        scoreText.gameObject.SetActive(true);
+        scoreText.DOFade(1f, 0.5f).From(0f);
+        
+        // Fade in juego y texto a la vez
+        sceneTitleText.transform.DOScale(1f, 1f).From(5f);
+        
+        
+        // Texto empieza en grande y lerpea hasta tamaño pequeño
+        
+        // Cuando el texto termina de lerpear, se agranda la view hasta dejar 100% view de la camara de la escena
+
+        yield return new WaitForEndOfFrame();
+    }
+
+    public IEnumerator TransitionOutMinigameCoroutine()
+    {
+        // Empezar a echar para atras camara
+        
+        // En 0.1f segundos, Fade to black
+        
+        // Update Score
+        
+        // Transicionar a otro juego
+        
+        yield return new WaitForEndOfFrame();
+    }
+
+    public void InitMinigame(int sceneIndex)
     {
         _currentSceneIndex = sceneIndex;
-        gameState = GameState.Playing;
         
-        gameScenes[_currentSceneIndex].Init();
+        gameScenes[_currentSceneIndex].Init(inputController);
         
         gameScenes[_currentSceneIndex].SceneWon += OnSceneWon;
         gameScenes[_currentSceneIndex].SceneLost += OnSceneLost;
+        
+        sceneTitleText.text = gameScenes[_currentSceneIndex].sceneTitle;
     }
     
     private void OnSceneWon()
@@ -53,7 +120,8 @@ public class MainController : MonoBehaviour
         gameScenes[_currentSceneIndex].SceneLost -= OnSceneLost;
         
         // Transition out
-        
+        StartCoroutine(TransitionOutMinigameCoroutine());
+
         _currentSceneIndex++;
         
         // Load the next scene
@@ -63,8 +131,7 @@ public class MainController : MonoBehaviour
             return;
         }
         
-        
-        StartMinigame(_currentSceneIndex);
+        InitMinigame(_currentSceneIndex);
         
         Debug.Log("CurrentSceneIndex " + _currentSceneIndex);
     }
--- Assets/_Project/_Scripts/Scenes/BaseScene.cs
+++ Assets/_Project/_Scripts/Scenes/BaseScene.cs
@@ -3,20 +3,45 @@ using UnityEngine;
 
 public abstract class BaseScene: MonoBehaviour
 {
-   [HideInInspector] public InputController inputController;
-   
    public Action SceneWon;
    public Action SceneLost;
-   
-   public float SceneDuration { get; protected set; }
-   public float TimeElapsedInScene;
 
-   public virtual void Init()
+   public Camera SceneCamera;
+   
+   public float sceneDuration = 5f;
+   public string sceneTitle = "Base Scene";
+   
+   private InputController inputController;
+   
+   private float timeElapsedInScene = 0f;
+   public bool IsSceneInit = false;
+   
+   public void Update()
+   {
+      if (IsSceneInit)
+      {
+         timeElapsedInScene += Time.deltaTime;
+      }
+      
+      if (timeElapsedInScene >= sceneDuration)
+      {
+         // END SCENE
+         Debug.Log("Scene Ended from timeout");
+
+         TimeoutEnded();
+      }
+   }
+
+   public virtual void Init(InputController inputControllerRef)
    {
       // Made sure we find the inputController even if we forget to assign it
-      if (!inputController)
+      if (!inputControllerRef)
       {
          inputController = FindObjectOfType<InputController>();
+      }
+      else
+      {
+         inputController = inputControllerRef;
       }
       
       inputController.KeyboardInputAction += OnKeyboardInputPressed;
@@ -26,7 +51,15 @@ public abstract class BaseScene: MonoBehaviour
       
       this.gameObject.SetActive(true);
       
+      IsSceneInit = true;
       Debug.Log("Loaded Scene with name: " + gameObject.name);
+   }
+   
+   // Method used whenever time ends. It may yield a positive or negative result
+   // depending on Scene Logic
+   protected virtual void TimeoutEnded()
+   {
+      
    }
    
    public virtual void Unload()
@@ -37,28 +70,25 @@ public abstract class BaseScene: MonoBehaviour
       inputController.MouseMoveAction -= OnMouseMove;
       
       this.gameObject.SetActive(false);
-
+      IsSceneInit = false;
+      
       Debug.Log("Unloaded Scene with name: " + gameObject.name);
    }
    
    protected virtual void OnMouseLeftClick()
    {
-      
    }
    
    protected virtual void OnMouseRightClick()
    {
-      
    }
    
    protected virtual void OnMouseMove(Vector2 obj)
    {
-      
    }
 
    protected virtual void OnKeyboardInputPressed(KeyCode keyPressed)
    {
-      
    }
    
    public virtual void Win()
