297990bea635032597347c171183906659fddd26
ArturoSerranoDev <asm.arturo.developer@gmail.com>
2023-07-15 14:16:00 +0200

    last update on develop cleaning up project and setting up endless runner

---
+++ Assets/_Project/_Scripts/EndlessRunnerRefs.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessRunnerRefs : MonoBehaviour
+{
+    public GameObject playerCharacter;
+    public GameObject rotatingWorld;
+
+    public GameObject piruli;
+    public GameObject torresKio;
+    public GameObject torresBlancas;
+}
--- Assets/_Project/_Scripts/Scenes/EndlessRunnerScene.cs
+++ Assets/_Project/_Scripts/Scenes/EndlessRunnerScene.cs
@@ -4,46 +4,56 @@ using UnityEngine;
 
 public class EndlessRunnerScene : BaseScene
 {
-       public override void Init(InputController inputControllerRef)
-       {
-          base.Init(inputControllerRef);
-       }
-       
-       // Method used whenever time ends. It may yield a positive or negative result
-       // depending on Scene Logic
-       protected override void TimeoutEnded()
-       {
-          
-       }
-       
-       public override void Unload()
-       {
-          base.Unload();
-       }
-       
-       protected override void OnMouseLeftClick()
-       {
-       }
-       
-       protected override void OnMouseRightClick()
-       {
-       }
-       
-       protected override void OnMouseMove(Vector2 obj)
-       {
-       }
-    
-       protected override void OnKeyboardInputPressed(KeyCode keyPressed)
-       {
-       }
-       
-       public override void Win()
-       {
-          SceneWon?.Invoke();
-       }
-    
-       protected override void Lose()
-       {
-          SceneLost?.Invoke();
-       }
+    // private GameObject playerCharacter;
+    // private GameObject playerCharacter;
+    private EndlessRunnerRefs _endlessRunnerRefs;
+
+    private GameObject playerCharacter;
+    private GameObject 
+    public override void Init(InputController inputControllerRef)
+    {
+        base.Init(inputControllerRef);
+        
+        _endlessRunnerRefs = FindObjectOfType<EndlessRunnerRefs>();
+        
+        
+    }
+
+    // Method used whenever time ends. It may yield a positive or negative result
+    // depending on Scene Logic
+    protected override void TimeoutEnded()
+    {
+
+    }
+
+    public override void Unload()
+    {
+        base.Unload();
+    }
+
+    protected override void OnMouseLeftClick()
+    {
+    }
+
+    protected override void OnMouseRightClick()
+    {
+    }
+
+    protected override void OnMouseMove(Vector2 obj)
+    {
+    }
+
+    protected override void OnKeyboardInputPressed(KeyCode keyPressed)
+    {
+    }
+
+    public override void Win()
+    {
+        SceneWon?.Invoke();
+    }
+
+    protected override void Lose()
+    {
+        SceneLost?.Invoke();
+    }
 }
